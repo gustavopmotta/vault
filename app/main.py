a210d47c8d0b3fc27eec271319b5757b064d4910
Gustavo <gugapmotta@gmail.com>
2026-09-17 17:48:01 -0300

    CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-	title="Vault API",
-	version="1.0.0",
+app = FastAPI(title="Vault API")
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.get("/")
